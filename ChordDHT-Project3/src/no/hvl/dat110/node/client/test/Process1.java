e500328c8b779975d0e7a707cf65024a5b8062ea
jhoffis <jhoffis@jhoffis-NB50TJ1-TK1>
2019-04-08 21:02:09 +0200

    isjofaoinj

--- ChordDHT-Project3/src/no/hvl/dat110/node/client/test/Process1.java
+++ ChordDHT-Project3/src/no/hvl/dat110/node/client/test/Process1.java
@@ -6,7 +6,7 @@ import no.hvl.dat110.rpc.ChordNodeContainer;
 public class Process1 {
 
 	public static void main(String[] args) throws Exception {
-		new ChordNodeContainer("process1", 50000, true);
+		new ChordNodeContainer(args[0], 50000, true);
 	}
 
 }
